815652d330649955c12038e153d0b222233e84cc
van long <vanlong11x1@gmail.com>
2022-04-07 14:51:56 +0700

    save view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Home\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
